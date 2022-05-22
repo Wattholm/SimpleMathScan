6dc768904c2583df275692d11495dc2cb566caba
Kevin Joseph Mangulabnan <kevinm2001ph@outlook.com>
2022-05-22 15:12:09 +0800

    Set up app function and theme via build configuration.

--- SimpleMathScan/BuildConfig.swift
+++ SimpleMathScan/BuildConfig.swift
@@ -7,30 +7,56 @@
 
 import Foundation
 
-enum Environment: String {
-    case debugGreenCamera = "Debug Green Camera"
-    case debugGreenRoll = "Debug Green Roll"
-    case debugRedCamera = "Debug Red Camera"
-    case debugRedRoll = "Debug Red Roll"
-    case releaseGreenCamera = "Release Green Camera"
-    case releaseGreenRoll = "Release Green Roll"
-    case releaseRedCamera = "Release Red Camera"
-    case releaseRedRoll = "Release Red Roll"
-    case unknown
-}
-
 class BuildConfig {
+
+    enum Environment: String {
+        case debugGreenCamera = "Debug Green Camera"
+        case debugGreenRoll = "Debug Green Roll"
+        case debugRedCamera = "Debug Red Camera"
+        case debugRedRoll = "Debug Red Roll"
+        case releaseGreenCamera = "Release Green Camera"
+        case releaseGreenRoll = "Release Green Roll"
+        case releaseRedCamera = "Release Red Camera"
+        case releaseRedRoll = "Release Red Roll"
+    }
+
+    enum AppFunction {
+        case camera
+        case roll
+    }
+
+    enum AppTheme {
+        case red
+        case green
+    }
+
     static let shared = BuildConfig()
     
     let environment: Environment
+    let appFunction: AppFunction
+    let appTheme: AppTheme
     
     init() {
         let currentConfiguration = Bundle.main.object(forInfoDictionaryKey: "Configuration") as! String
         
-        environment = Environment(rawValue: currentConfiguration) ?? .unknown
-        
-        guard environment != .unknown else {
+        guard let environment = Environment(rawValue: currentConfiguration) else {
             fatalError("Unknown build configuration")
+        }
+        
+        self.environment = environment
+        
+        switch environment {
+        case .debugGreenCamera, .releaseGreenCamera, .debugRedCamera, .releaseRedCamera:
+            appFunction = .camera
+        case .debugGreenRoll, .releaseGreenRoll, .debugRedRoll, .releaseRedRoll:
+            appFunction = .roll
+        }
+
+        switch environment {
+        case .debugGreenCamera, .releaseGreenCamera, .debugGreenRoll, .releaseGreenRoll:
+            appTheme = .green
+        case .debugRedCamera, .releaseRedCamera, .debugRedRoll, .releaseRedRoll:
+            appTheme = .red
         }
     }
 }
